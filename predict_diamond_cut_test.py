ee36e940abde7318ffc604fba133bc9d8673171d
mossdet <mossdet.detector@gmail.com>
2024-04-15 09:44:27 -0600

    Loop through test set and send requests to classifier. Update description of web service commands.

--- predict_diamond_cut_test.py
+++ predict_diamond_cut_test.py
@@ -1,6 +1,7 @@
 import requests
 import pickle
 import pandas as pd
+import random
 
 from ml_framework.data_classification.run_eda_classification import ClassificationEDA
 
@@ -10,34 +11,39 @@ with open(analyzer_filepath, 'rb') as f_in:
     data_analyzer= pickle.load(f_in)
 
 
-test_data_idx = 30
-
 data_to_predict = data_analyzer.test_data
 
-diamond_data = {
-    "carat":data_to_predict.iloc[test_data_idx,0],
-    "cut":data_to_predict.iloc[test_data_idx,1],
-    "color":data_to_predict.iloc[test_data_idx,2],
-    "clarity":data_to_predict.iloc[test_data_idx,3],
-    "depth":data_to_predict.iloc[test_data_idx,4],
-    "table":data_to_predict.iloc[test_data_idx,5],
-    "price":data_to_predict.iloc[test_data_idx,6],
-    "x":data_to_predict.iloc[test_data_idx,7],
-    "y":data_to_predict.iloc[test_data_idx,8],
-    "z":data_to_predict.iloc[test_data_idx,9],
-}
-
-print(diamond_data)
-
-url = "http://192.168.4.20:9696/predict_diamond_cut"
-url = "http://172.30.176.211:9696/predict_diamond_cut"
+#url = "http://192.168.4.20:9696/predict_diamond_cut"
+#url = "http://172.30.176.211:9696/predict_diamond_cut"
 url = "http://0.0.0.0:9696/predict_diamond_cut"
+url = "http://10.96.95.250:80/predict_diamond_cut"
 
-diamond_cut_predictions = requests.post(url, json=diamond_data).json()
+#for test_data_idx in range(len(data_analyzer.test_data)):
+for test_data_idx in range(10):
+    #test_data_idx = random.randint(0, len(data_analyzer.test_data))
+    print("Test data entree idx: ", test_data_idx)
 
-print("\nDiamond Cut Predictions:")
-for k,v in diamond_cut_predictions.items():
-    print(k,v)
+    diamond_data = {
+        "carat":data_to_predict.iloc[test_data_idx,0],
+        "cut":data_to_predict.iloc[test_data_idx,1],
+        "color":data_to_predict.iloc[test_data_idx,2],
+        "clarity":data_to_predict.iloc[test_data_idx,3],
+        "depth":data_to_predict.iloc[test_data_idx,4],
+        "table":data_to_predict.iloc[test_data_idx,5],
+        "price":data_to_predict.iloc[test_data_idx,6],
+        "x":data_to_predict.iloc[test_data_idx,7],
+        "y":data_to_predict.iloc[test_data_idx,8],
+        "z":data_to_predict.iloc[test_data_idx,9],
+    }
 
+    print(diamond_data)
+
+    diamond_cut_predictions = requests.post(url, json=diamond_data).json()
+
+    print("Diamond Cut Predictions:")
+    for k,v in diamond_cut_predictions.items():
+        print(k,v)
+
+    print("\n\n")
 
 
